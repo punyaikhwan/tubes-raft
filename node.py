367962068981fe67be0c568736f7b377f9b137cc
namaldung <13514052@std.stei.itb.ac.id>
2017-04-27 21:31:59 +0700

    implementasi bbrp prosedur

--- node.py
+++ node.py
@@ -1,18 +1,20 @@
+import urllib2
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 
-class Node():
+class Node(BaseHTTPRequestHandler):
     def __init__(self, node_id, timeout = 1, list_worker = []):
     # create objek
-        self.node_id = node_id
+        self.node_id = node_id # sekaligus jadi port nya
         self.timeout = timeout
         self.list_worker = list_worker
         self.list_workerLoad = [0] * len(list_worker)
 
-    def initialize(self, leader_id, list_node = []):
+    def initialize(self, leader_id, list_nodeID = []):
     # node pertama kali dinyalakan
         self.leader_id = leader_id
-        self.list_node = list_node
-        self.list_isNodeAlive = [True] * len(list_node)
+        self.list_nodeID = list_nodeID
+        self.list_isNodeAlive = [True] * len(list_nodeID)
+        self.isAlive = True
         # jalankan peran leader/follower
         if (self.leader_id == self.node_id):
             self.leaderMain()
@@ -21,27 +23,63 @@ class Node():
 
     def leaderMain(self):
     # program utama ketika berperan sebagai leader
-        return
+        while (True):
+            if (self.isAlive):
+                return
 
     def followerMain(self):
     # program utama ketika berperan sebagai follower
-        return
+        while (True):
+            if (self.isAlive):
+                return
 
     def candidateMain(self):
     # program utama ketika berperan sebagai candidate leader
-        return
+        while (True):
+            if (self.isAlive):
+                return
 
-    def doGet(self, data = None, list_dest = []):
-    # mengirim request/response
-        return
+    def sendResponse(self, data = None):
+        self.send_response(200) # 200 = OK
+        self.end_headers()
+        self.wfile.write(str(data).encode('utf-8'))
+
+    def do_Get(self, data = None, list_dest = []):
+    # menghandle request HTTP GET yang masuk ke node ini
+        try:
+            args = self.path.split('/')
+            # mengecek siapa yang mengirim request
+            if (len(args) == 2):
+                command = args[1]
+                if (command == 'heartbeat'): # dari leader
+                    self.sendHeartbeatResponse()
+                elif (command == 'vote'): # dari candidate
+                    self.sendVoteResponse()
+                elif (command == 'pause'):
+                    self.pause()
+                elif (command == 'resume'):
+                    self.resume()
+            elif (len(args) == 3):
+                command = args[1]
+                n = int(args[2])
+                if (command == 'prime'): # dari client
+                    self.sendPrimeRequest(n)
+            else:
+                raise Exception()
+        except Exception as ex:
+            self.send_response(500) # 500 = Internal Server Error
+            self.end_headers()
 
     def sendHeartbeat(self):
     # mengirim heartbeat, dilakukan oleh leader
-        return
+        for nodeID in list_nodeID:
+            if (nodeID != self.node_id):
+                urllib2.urlopen("http://localhost:" + nodeID + "/heartbeat")
 
     def sendHeartbeatResponse(self):
     # mengirim heartbeat response, dilakukan oleh follower/candidate lain
-        return
+        # membaca pesan pada heartbeat
+        rawdata = self.rfile.read(int(self.headers.getheader('Content-Length')))
 
     def sendVoteRequest(self):
     # mengirim vote, dilakukan oleh candidate
@@ -49,17 +87,26 @@ class Node():
 
     def sendVoteResponse(self):
     # mengirim vote response, dilakukan oleh follower
-        return
+        # membaca pesan pada vote request
+        rawdata = self.rfile.read(int(self.headers.getheader('Content-Length')))
 
     def pause(self):
     # simulasi ketika node mati
-        return
+        if (self.isAlive):
+            isAlive = False
+            self.sendResponse('Node ' + self.node_id + ' now down.')
+        else:
+            self.sendResponse('Node ' + self.node_id + ' already down.')
 
     def resume(self):
     # simulasi ketika node hidup kembali
-        return
+        if (self.isAlive):
+            self.sendResponse('Node ' + self.node_id + ' already up.')
+        else:
+            isAlive = True
+            self.sendResponse('Node ' + self.node_id + ' now up.')
 
-    def sendPrimeRequest(self):
+    def sendPrimeRequest(self, n):
     # meminta bilangan prima ke worker, dilakukan oleh leader/follower
         return
 
@@ -69,13 +116,15 @@ maxload = 1000 # jika worker mati maka elemen ybs pada list_workerLoad = maxload
 # VARIABLE GLOBAL
 list_worker = []
 list_node = []
+list_nodeID = []
 n_node = 5 # hardcode
 
 # PROGRAM UTAMA
 for i in range(n_node):
     node = Node(i, i, list_worker)
     list_node.append(node)
+    list_nodeID.append(i)
 
-for i in range(n_node):
+for node in list_node:
     # penentuan leader pertama kali di hardcode
-    node.initialize(1, list_node)
+    node.initialize(1, list_nodeID)
