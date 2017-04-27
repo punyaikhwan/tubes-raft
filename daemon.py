74d2ad010d90cf89dad85ab22e3720cee38df99c
Ikhwanul Muslimin <ikhwan.m1996@gmail.com>
2017-04-27 17:04:51 +0700

    add active send heartbeat from daemon

--- daemon.py
+++ daemon.py
@@ -1,17 +1,20 @@
 '''Kelas Daemon
 Berfungsi mengirimkan hearthbeat berupa ID dan CPU usage.
-Informasi ini dikirim jika node meminta.
+Informasi dikirim setiap interval waktu.
 Caranya, lakukan pemanggilan ke IP address:port'''
 
 from __future__ import print_function
 import os
 import psutil
 import socket
+import time
 from BaseHTTPServer import HTTPServer
 from BaseHTTPServer import BaseHTTPRequestHandler
 PORT = 13336
+interval = 2
 
 class DaemonHandler(BaseHTTPRequestHandler):
+
     def getCPUusage(self):
         return psutil.cpu_percent(interval=1)
 
@@ -25,9 +28,11 @@ class DaemonHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         try:
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(str(self.sendHeartBeat()).encode('utf-8'))
+            while True:
+                self.send_response(200)
+                self.end_headers()
+                self.wfile.write(str(self.sendHeartBeat()).encode('utf-8'))
+                time.sleep(interval)
         except Exception as ex:
             self.send_response(500)
             self.end_headers()
